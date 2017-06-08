b4cc21bf0cd31324e5388dceef9ce567b1fbd383
koban6 <kobayashi6y@gmail.com>
2017-06-08 23:53:22 +0900

    Add Comment (DynamicKomi.cpp DynamicKomi.h)

--- src/DynamicKomi.h
+++ src/DynamicKomi.h
@@ -8,33 +8,37 @@
 //    定数    //
 ////////////////
 
-// コミを緩める勝率の閾値
+//  コミを緩める勝率の閾値
 const double RED = 0.35;
-// コミを厳しくする勝率の閾値
+
+//  コミを厳しくする勝率の閾値
 const double GREEN = 0.75;
 
-// コミの値を収束させる手数
+//  コミの値を収束させる手数
 const int LINEAR_THRESHOLD = 200;
-// 置き石1子あたりの重み
+
+//  置き石1子あたりの重み
 const int HANDICAP_WEIGHT = 8;
 
+//  Dynamic Komiの設定
 enum DYNAMIC_KOMI_MODE {
   DK_OFF,
   DK_LINEAR,
   DK_VALUE,
 };
 
+
 ////////////////
 //    関数    //
 ////////////////
 
-// 置き石の個数の設定
+//  置き石の個数の設定
 void SetHandicapNum( const int num );
 
-// 置き石の個数の設定(テスト対局用)
+//  置き石の個数の設定(テスト対局用)
 void SetConstHandicapNum( const int num );
 
-// Dynamic Komi
+//  Dynamic Komi
 void DynamicKomi( const game_info_t *game, const uct_node_t *root, const int color );
 
 #endif
