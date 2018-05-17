198964cdda8584698e224a905d20a97c813d6b50
kobanium <rayauthor19x19@gmail.com>
2018-05-17 21:53:07 +0900

    Add Comment

--- src/SgfExtractor.h
+++ src/SgfExtractor.h
@@ -11,22 +11,24 @@ struct SGF_record_t {
   int handicap_x[20];   // 置き石(x座標)
   int handicap_y[20];   // 置き石(y座標)
   int board_size;       // 盤のサイズ
-  char black_name[256];
-  char white_name[256];
+  char black_name[256]; // 黒番のプレイヤーの名前
+  char white_name[256]; // 白番のプレイヤーの名前
   double komi;          // コミ
 };
 
 
 enum KIFU_RESULT {
-  R_JIGO,
-  R_BLACK,
-  R_WHITE,
-  R_UNKNOWN,
+  R_JIGO,    // 持碁
+  R_BLACK,   // 黒勝ち
+  R_WHITE,   // 白勝ち
+  R_UNKNOWN, // 不明
 };
 
 
+// SGFファイルの読み込み
 void ExtractKifu( const char *file_name, SGF_record_t *kifu );
 
+// 着手を抽出
 int GetKifuMove( const SGF_record_t *kifu, int n );
 
 #endif
