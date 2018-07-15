fc9a7909026116734979fe7e3b181a56b68edadf
kobanium <rayauthor19x19@gmail.com>
2018-07-15 23:58:09 +0900

    Refactoring

--- src/Rating.cpp
+++ src/Rating.cpp
@@ -42,7 +42,7 @@ static float po_tactical_set2[PO_TACTICALS_MAX2];
 char po_params_path[1024];
 
 // ビットマスク
-const unsigned int po_tactical_features_mask[F_MASK_MAX] = {
+static const unsigned int po_tactical_features_mask[F_MASK_MAX] = {
   0x00000001,  0x00000002,  0x00000004,  0x00000008,
   0x00000010,  0x00000020,  0x00000040,  0x00000080,
   0x00000100,  0x00000200,  0x00000400,  0x00000800,
